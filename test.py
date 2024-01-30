a80798352f462f78f35b90bc2edebdc25f600fa7
sanju0745 <127760968+sanju0745@users.noreply.github.com>
2024-01-30 19:20:28 +0100

    test 3

---
+++ test.py
@@ -0,0 +1,12 @@
+# Define the directory path and file name
+directory_path = './eventB'
+file_name = 'hello_world.txt'
+
+# Combine the directory path and file name to create the full file path
+file_path = f'{directory_path}/{file_name}'
+
+# Write "Hello, World!" to the file
+with open(file_path, 'w') as file:
+    file.write('Hello, World!')
+
+print(f'File "{file_name}" created in directory: {directory_path}')
