cc9c705b15fa6c8e1b56dd79f75b0d4bf786f9c5
sanju0745 <127760968+sanju0745@users.noreply.github.com>
2024-01-30 19:47:04 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+# test.py
 # Define the directory path and file name
 directory_path = './eventB/Images'
 file_name = 'hello_world.txt'
@@ -5,8 +6,9 @@ file_name = 'hello_world.txt'
 # Combine the directory path and file name to create the full file path
 file_path = f'{directory_path}/{file_name}'
 
-# Write "Hello, World!" to the file
-with open(file_path, 'w') as file:
-    file.write('Hello, World!')
+if __name__ == "__main__":
+    # Write "Hello, World!" to the file
+    with open(file_path, 'w') as file:
+        file.write('Hello, World!')
 
-print(f'File "{file_name}" created in directory: {directory_path}')
+    print(f'File "{file_name}" created in directory: {directory_path}')
