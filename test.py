4693345c6f85443eea4dd0c0f52fb687b7516332
sanju0745 <127760968+sanju0745@users.noreply.github.com>
2024-01-30 19:58:19 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 # test.py
+import os
+
 # Define the directory path and file name
 directory_path = './eventB/Images'
 file_name = 'hello_world.txt'
@@ -7,6 +9,10 @@ file_name = 'hello_world.txt'
 file_path = f'{directory_path}/{file_name}'
 
 if __name__ == "__main__":
+    # Check if the directory exists, create it if not
+    if not os.path.exists(directory_path):
+        os.makedirs(directory_path)
+
     # Write "Hello, World!" to the file
     with open(file_path, 'w') as file:
         file.write('Hello, World!')
